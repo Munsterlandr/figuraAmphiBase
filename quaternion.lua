9b4bcfe2a3e861aa8f16563448048d866263f482
munsterlandr <corgi47poke@gmail.com>
2024-04-25 17:57:41 -0600

    changed Quaternion conversion system to one i found on the net

--- quaternion.lua
+++ quaternion.lua
@@ -1,9 +1,9 @@
 -- versors are unit quaternions, ie of length 1. They're used for rotations in computer graphics n such because they're more efficient to compute than matrices.
 
 Quaternion = {}
-function Quaternion.new(real, i, j, k)
+function Quaternion.new(n, i, j, k)
     local o = {}
-    o.real = real
+    o.n = n
     o.i = i
     o.j = j
     o.k = k
@@ -14,50 +14,75 @@ end function Quaternion.byAxisAngle(theta, axis)
     local sinTheta = math.sin(thetaRad/2)
     return Quaternion.new(math.cos(thetaRad/2), axis.x * sinTheta, axis.y * sinTheta, axis.z * sinTheta)
 end function Quaternion.byTaitBryan(vect)
-    local rollRads = math.rad(vect.z)
-    local yawRads = math.rad(vect.y)
-    local axis = vec(math.cos(rollRads) * math.cos(-yawRads), math.sin(rollRads) * math.cos(-yawRads), math.sin(-yawRads))
-    print(axis)
-    return Quaternion.byAxisAngle(vect.x, axis)
+    local halfPitch = math.rad(vect.x)/2
+    local sinHalfPitch = math.sin(halfPitch)
+    local cosHalfPitch = math.cos(halfPitch)
+    local halfYaw = math.rad(vect.y)/2
+    local sinHalfYaw = math.sin(halfYaw)
+    local cosHalfYaw = math.cos(halfYaw)
+    local halfRoll = math.rad(vect.z)/2
+    local sinHalfRoll = math.sin(halfRoll)
+    local cosHalfRoll = math.cos(halfRoll)
+
+    return Quaternion.new(
+        cosHalfPitch*cosHalfYaw*cosHalfRoll + sinHalfPitch*sinHalfYaw*sinHalfRoll,
+        sinHalfPitch*cosHalfYaw*cosHalfRoll - cosHalfPitch*sinHalfYaw*sinHalfRoll,
+        cosHalfPitch*sinHalfYaw*cosHalfRoll + sinHalfPitch*cosHalfYaw*sinHalfRoll,
+        cosHalfPitch*cosHalfYaw*sinHalfRoll - sinHalfPitch*sinHalfYaw*cosHalfRoll
+    )
+end function Quaternion.threeStepFromTB(vect) -- todo
+    -- to axis-angle
+
+
+    -- to Versor
+
 end
 function Quaternion:copy()
     return Quaternion.new(self.real, self.i, self.j, self.k)
 end
 function Quaternion:toVec4()
-    return vec(self.i,self.j,self.k, self.real)
+    return vec(self.i,self.j,self.k, self.n)
 end function Quaternion:getLength()
-    return math.sqrt(self.real^2 + self.i^2 + self.j^2 + self.k^2)
+    return math.sqrt(self.n^2 + self.i^2 + self.j^2 + self.k^2)
+end function Quaternion:getAngle()
+    return math.deg(math.acos(self.n))*2
 end
 -- Quaternion to other rotation functions, currently a bit borked
 function Quaternion:toAxisAngle()
-    local radAngle = math.acos(self.real)
+    local radAngle = math.acos(self.n)
     local sinAngle = math.sin(radAngle)
     local angle = math.deg(radAngle) * 2
-    local axis = vec(self.i, self.j, self.k)/vec(self.i,self.k,self.j):length()
+    local axis = vec(self.i, self.j, self.k)/sinAngle
     return angle, axis
 end function Quaternion:toTaitBryan()
-    local angle, axis = self:toAxisAngle()
+    local pitch
+    local yaw -- this is the only non-locking term
+    local roll
 
-    local minusYawRad = math.asin(axis.z)
-    local cosMinusYaw = math.cos(minusYawRad)
-    local cosRoll = axis.x/cosMinusYaw
-    local sinRoll = axis.y/cosMinusYaw
-    
-    local radRollBySin = math.asin(sinRoll)
-    local radRollByCos = math.acos(cosRoll)
-    print(radRollBySin)
-    print(radRollByCos)
+    yaw = math.deg( math.asin(2*(self.n*self.i + self.j*self.k)) )
+    if math.abs(yaw) == 90 then
+        pitch = 0
+        roll = -2*math.deg( math.atan2(self.i, self.n) )
+        if yaw == -90 then
+            roll = -roll
+        end
+    else
+        -- define pitch
+        pitch = math.deg( math.atan2(2*(self.n*self.i + self.j*self.k), self.n^2 - self.i^2 - self.j^2 + self.k^2) )
 
-    if true then
-        return(vec(angle,-math.deg(minusYawRad),math.deg(math.asin(sinRoll))))
+        -- define roll
+        roll = math.deg( math.atan2(2*(self.n*self.k - self.i*self.j), self.n^2+self.i^2-self.j^2-self.k^2) )
     end
+
+    -- return the thing
+    return vec(pitch, yaw, roll)
 end function Quaternion:invert()
     self.i = -self.i
     self.j = -self.j
     self.k = -self.k
 end
 function Quaternion.__tostring(self)
-    local str = ""..self.real
+    local str = ""..self.n
     if self.i < 0 then
         str = str.."-i"
     else
@@ -78,10 +103,10 @@ function Quaternion.__tostring(self)
     return str..math.abs(self.k)
 end function Quaternion.__mul(a, b)
     local c = Quaternion.new(0,0,0,0)
-    c.real = (a.real * b.real) - (a.i * b.i) - (a.j * b.j) - (a.k * b.k)
-    c.i    = (a.real * b.i) + (a.i * b.real) + (a.j * b.k) - (a.k * b.j)
-    c.j    = (a.real * b.j) - (a.i * b.k) + (a.j * b.real) + (a.k * b.i)
-    c.k    = (a.real * b.k) + (a.i * b.j) - (a.j * b.i) + (a.k * b.real)
+    c.n = (a.n * b.n) - (a.i * b.i) - (a.j * b.j) - (a.k * b.k)
+    c.i = (a.n * b.i) + (a.i * b.n) + (a.j * b.k) - (a.k * b.j)
+    c.j = (a.n * b.j) - (a.i * b.k) + (a.j * b.n) + (a.k * b.i)
+    c.k = (a.n * b.k) + (a.i * b.j) - (a.j * b.i) + (a.k * b.n)
     return c
 end
 Quaternion.__index = Quaternion
